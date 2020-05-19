39e480d92f48b2dec199123e8f8aa01006feef85
Migara Ekanayake <mekanayake@paloaltonetworks.com>
2020-05-19 02:36:33 +0100

    add route_table support

--- main.tf
+++ main.tf
@@ -39,7 +39,7 @@ resource "aws_route_table_association" "this" {
 resource "aws_ec2_transit_gateway_vpc_attachment" "this" {
   count              = lookup(var.tgw_vpc_attachment, "tgw_id", null) != null ? 1 : 0
   vpc_id             = aws_vpc.this.id
-  subnet_ids         = [for subnet in var.tgw_vpc_attachment : aws_subnet.this[subnet].id]
+  subnet_ids         = [for subnet in var.tgw_vpc_attachment.subnets : aws_subnet.this[subnet].id]
   transit_gateway_id = var.tgw_vpc_attachment.tgw_id
 
   transit_gateway_default_route_table_association = false
